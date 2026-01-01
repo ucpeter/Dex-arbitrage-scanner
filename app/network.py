d947f3e433f0d2bc234643435424f979b14065f4
ucpeter <chucks0606@gmail.com>
2026-01-01 22:09:41 +0100

    Create network.py

---
+++ app/network.py
@@ -0,0 +1,7 @@
+RPC_ENDPOINTS = {
+    "ethereum": "https://eth.llamarpc.com",
+    "arbitrum": "https://arbitrum.llamarpc.com",
+    "optimism": "https://optimism.llamarpc.com",
+    "polygon": "https://polygon.llamarpc.com",
+    "base": "https://base.llamarpc.com",
+}
