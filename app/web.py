851b5f3c36e2909e060d3ec56d33db4098c3c86c
ucpeter <chucks0606@gmail.com>
2026-01-01 22:28:51 +0100

    Create web.py

---
+++ app/web.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from app.scanner import scan
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+@app.post("/scan", response_class=HTMLResponse)
+def run_scan(request: Request, network: str = Form(...)):
+    results = scan(network)
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "results": results
+        }
+    )
