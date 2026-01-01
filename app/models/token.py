7d93e0334e0329f24e81209988c8674d42b6c584
ucpeter <chucks0606@gmail.com>
2026-01-01 22:13:46 +0100

    Update and rename app/models/token.py to app/token.py

--- app/models/token.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class Token:
-    symbol: str
-    address: str
-    decimals: int
-    chain: str
---
+++ app/token.py
@@ -0,0 +1,14 @@
+
+TOKENS = {
+    "ethereum": [
+        "WETH","USDC","USDT","DAI","WBTC","LINK","UNI","AAVE","CRV","LDO",
+        "MKR","SNX","BAL","COMP","SUSHI","1INCH","ENS","GMX","PENDLE","RPL",
+        "CVX","FXS","GALA","APE","PEPE","BLUR","MEME","ARB","OP","RNDR",
+        "IMX","DYDX","STG","ZRX","BAND","ILV","YFI","NMR","AUDIO","ACH",
+        "LOOM","API3","SPELL","FET","OCEAN","GRT","CTSI","ANKR","SKL","CELR"
+    ],
+    "base": [
+        "WETH","USDC","AERO","DEGEN","BRETT","BALD","MOG","TOSHI","FRIEND",
+        "CBETH"
+    ]
+}
