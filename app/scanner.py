7aef1fc8509e1718cb211901105d672858fb8948
ucpeter <chucks0606@gmail.com>
2026-01-02 10:45:51 +0100

    Update scanner.py

--- app/scanner.py
+++ app/scanner.py
@@ -1,6 +1,6 @@
 import json
 from.tokens import TOKENS
-from app.config import MIN_GROSS_PROFIT_PCT, ESTIMATED_GAS_PCT, OUTPUT_FILE
+from.config import MIN_GROSS_PROFIT_PCT, ESTIMATED_GAS_PCT, OUTPUT_FILE
 from app.uniswap_v3 import get_uniswap_price
 from app.paraswap_v5 import get_paraswap_price
 
