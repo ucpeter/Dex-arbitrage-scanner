28e1e517b5b35762e93195033a89d8aa18481dd9
ucpeter <chucks0606@gmail.com>
2026-01-04 17:21:12 +0100

    Update web.py

--- app/web.py
+++ app/web.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from .scanner import run_scan as scan_network
+from app.scanner import run_scan as scan_network
 
 app = FastAPI()
 
